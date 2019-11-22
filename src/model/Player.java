e0252df3deacee0fc4d8906bb93505089302c9a9
Chansocheat Chheang <chansocheatchheang@gmail.com>
2019-11-22 20:51:11 +1300

    initial commit

---
+++ src/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Player {
+
+    private String name;
+    private int level;
+
+    /**
+     * The player of the game upon creation.
+     * @param name name of the player
+     */
+    public Player(String name) {
+        this.name = name;
+        this.level = level;
+    }
+}
